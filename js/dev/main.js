248922ecdfe408bcd7e1e1cc2513fc223dcc9b9c
mihalichpalich <pavlovtfspb@gmail.com>
2017-11-09 23:18:19 +0300

    добавил переключатель режимов отображения товаров

--- js/dev/main.js
+++ js/dev/main.js
@@ -1,1 +1,12 @@
-var catalogViewToogle = document.querySelectorAll("");
+var catalogViewToogle = document.querySelectorAll(".catalog_view__item");
+
+for (var i=0; i<catalogViewToogle.length; i++) {
+  catalogViewToogle[i].addEventListener('click', function(e) {
+    var elem = e.currentTarget;
+
+    if(!elem.classList.contains('catalog_view__item--active')) {
+      document.querySelector(".catalog_view__item--active").classList.remove('catalog_view__item--active');
+      elem.classList.add('catalog_view__item--active');
+    }
+  });
+}
